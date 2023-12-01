59ae1d8640db1dd8d7181d5206b51b3f7364ba45
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-12-02 00:34:28 +0900

    TextureComponent 내용 추가

--- Util/TextureComponent.h
+++ Util/TextureComponent.h
@@ -1,5 +1,39 @@
 #pragma once
-class TextureComponent
-{
+
+class TextureComponent {
+public:
+	TextureComponent();
+	~TextureComponent();
+
+private:
+	std::vector<TextureInfo> m_textures{ };
+	std::vector<CubeMapInfo> m_cubeMaps{ };
+
+public:
+	void SetTextureDefaultOption();
+	void LoadTexture(const std::string& textureFilePath, bool flipImageOnLoad);
+
+	void BindingTexture(int textureIndex);
+	void UnBindingTextures();
+
+	uint32 GetTextureID(int textureIndex);
+	const TextureInfo& GetTextureInfo(int textureIndex) const;
 };
 
+class CubeMapTexture {
+public:
+	CubeMapTexture();
+	~CubeMapTexture();
+
+private:
+	std::vector<CubeMapInfo> m_cubeMaps{ };
+
+public:
+	void SetCubeMapTextureOption();
+
+	void LoadCubeMapTexture(const std::string& posX, const std::string& negX, const std::string& posY,
+		const std::string& negY, const std::string& posZ, const std::string& negZ, bool);
+
+	void BindCubeMap(int cubeMapIndex);
+	void UnBindCubeMap();
+};
