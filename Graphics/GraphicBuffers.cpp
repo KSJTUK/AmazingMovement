c54facc79931532c8541890b41971d1ae47673ad
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-12-02 00:38:26 +0900

    GraphicBuffers내용 추가

--- Graphics/GraphicBuffers.cpp
+++ Graphics/GraphicBuffers.cpp
@@ -1,2 +1,82 @@
 #include "pch.h"
-#include "GraphicBuffers.h"
+#include "Graphics/GraphicBuffers.h"
+
+GraphicBuffers::GraphicBuffers() : m_drawMode{ GL_TRIANGLES } { }
+
+GraphicBuffers::~GraphicBuffers() {
+	glDeleteVertexArrays(1, &m_vertexArray);
+	glDeleteBuffers(1, &m_vertexBuffer);
+}
+
+void GraphicBuffers::Init() {
+	// VAO 객체 생성 및 바인드
+	// VBO 객체 생성 및 바인드
+	glGenVertexArrays(1, &m_vertexArray);
+	glGenBuffers(1, &m_vertexBuffer);
+
+	glBindVertexArray(m_vertexArray);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+}
+
+void GraphicBuffers::ResetVerticies(const std::vector<Vertex>& verticies) {
+	m_vertexDataSize = verticies.size();
+
+	glDeleteVertexArrays(1, &m_vertexArray);
+
+	glGenVertexArrays(1, &m_vertexArray);
+	glGenBuffers(1, &m_vertexBuffer);
+
+	glBindVertexArray(m_vertexArray);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+
+	glBufferData(GL_ARRAY_BUFFER, m_vertexDataSize * sizeof(Vertex), &verticies[0], GL_STATIC_DRAW);
+
+	// location 0번에 Vertex객체의 position정보를 넘겨줌
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+	glEnableVertexAttribArray(0);
+
+	// location 1번에 Vertex객체의 texture정보를 넘겨줌
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texture));
+	glEnableVertexAttribArray(1);
+
+	// location 2번에 Vertex객체의 normal정보를 넘겨줌
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+	glEnableVertexAttribArray(2);
+}
+
+void GraphicBuffers::SetVerticies(const std::vector<Vertex>& verticies) {
+	m_vertexDataSize = verticies.size();
+
+	// Vertex객체의 정보를 VBO에 넘겨줌
+	glBindVertexArray(m_vertexArray);
+	glBufferData(GL_ARRAY_BUFFER, m_vertexDataSize * sizeof(Vertex), &verticies[0], GL_STATIC_DRAW);
+
+	// location 0번에 Vertex객체의 position정보를 넘겨줌
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+	glEnableVertexAttribArray(0);
+
+	// location 1번에 Vertex객체의 texture정보를 넘겨줌
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texture));
+	glEnableVertexAttribArray(1);
+
+	// location 2번에 Vertex객체의 normal정보를 넘겨줌
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+	glEnableVertexAttribArray(2);
+	glBindVertexArray(0);
+}
+
+void GraphicBuffers::SetPatchParameters(int numOfPatches) {
+	glPatchParameteri(GL_PATCH_VERTICES, numOfPatches);
+}
+
+void GraphicBuffers::SetDrawMode(unsigned int mode) {
+	m_drawMode = mode;
+}
+
+void GraphicBuffers::Render() {
+	// shaderProgram 에서 UseProgram을 활성화 했다는 가정하에 수행
+	glBindVertexArray(m_vertexArray);
+	glDrawArrays(m_drawMode, 0, uint32(m_vertexDataSize));
+	glBindVertexArray(0); // Array 바인드 해제
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
--- Graphics/GraphicBuffers.h
+++ Graphics/GraphicBuffers.h
@@ -1,5 +1,33 @@
 #pragma once
-class GraphicBuffers
-{
-};
 
+// VAO, VBO, EBO를 바인딩해서 삼각형을 그려줄 클래스
+class GraphicBuffers {
+public:
+	GraphicBuffers();
+	~GraphicBuffers();
+
+private:
+	// Vertex Buffer Object ID
+	unsigned int m_vertexBuffer{ };
+
+	// Vertex Array Object ID
+	unsigned int m_vertexArray{ };
+
+	size_t m_vertexDataSize{ };
+	size_t m_indexDataSize{ };
+
+	// 그리기 모드 점, 선, 삼각형 등
+	// default값은 삼각형(생성자에서 설정)
+	unsigned int m_drawMode{ };
+
+public:
+	void Init();
+
+	void ResetVerticies(const std::vector<Vertex>& verticies);
+
+	void SetVerticies(const std::vector<Vertex>& verticies);
+	void SetPatchParameters(int numOfPatches);
+
+	void SetDrawMode(unsigned int mode);
+	void Render();
+};
