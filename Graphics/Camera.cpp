b8ccef5ec2b3f8f075b0484675ab6f361840c38c
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-12-02 00:57:59 +0900

    Graphics에 필요한 파일 내용들 추가

--- Graphics/Camera.cpp
+++ Graphics/Camera.cpp
@@ -1,2 +1,143 @@
 #include "pch.h"
-#include "Camera.h"
+#include "Shader.h"
+#include "Graphics/Camera.h"
+
+const glm::mat4& Camera::GetViewMat() const {
+	return m_view;
+}
+
+void Camera::CameraPositionSet(const glm::vec3& position) {
+	m_EYE = position;
+}
+
+void Camera::CameraViewPointSet(const glm::vec3& at) {
+	m_AT = at;
+	if (m_AT == glm::vec3{ 0.f }) {
+		m_AT = glm::vec3{ 0.f, 0.f, 1.f };
+	}
+}
+
+void Camera::CameraMoveSpeedSet(const float& speed) {
+	m_moveSpeed = speed;
+}
+
+void Camera::ViewPointFix() {
+	m_fixAt = 0.f;
+}
+
+void Camera::ViewPointUnFix() {
+	m_fixAt = 1.f;
+}
+
+void Camera::CameraPositionRotateX(const float& angle, const glm::vec3& center) {
+	//ObjectMove::OrbitMove(m_EYE, angle, ObjectMove::axisY, center);
+}
+
+void Camera::Input(unsigned char key, bool down) {
+	switch (key) {
+	case 'm':
+		Move(m_cameraAxisY);
+		break;
+
+	case 'n':
+		Move(-m_cameraAxisY);
+		break;
+
+	case 'd':
+		Move(m_cameraAxisX);
+		break;
+
+	case 'a':
+		Move(-m_cameraAxisX);
+		break;
+
+	case 'w':
+		Move(-m_cameraAxisZ);
+		break;
+
+	case 's':
+		Move(m_cameraAxisZ);
+		break;
+	}
+}
+
+void Camera::SpecialInput(int key, bool down) {
+}
+
+void Camera::MouseMotionInput(int x, int y, int prevX, int prevY) {
+	float angleX{ -(x - prevX) / m_angleSpeed };
+	ViewPointMove(angleX, m_cameraAxisY);
+	m_cameraAngleX += angleX;
+
+	if (m_cameraAngleY > -90.f and m_cameraAngleY < 90.f) {
+		float angleY{ -(y - prevY) / m_angleSpeed };
+		ViewPointMove(angleY, m_cameraAxisX);
+		m_cameraAngleY += angleY;
+	}
+	else if (m_cameraAngleY <= -90.f) {
+		m_cameraAngleY = -89.f;
+	}
+	else if (m_cameraAngleY >= 90.f) {
+		m_cameraAngleY = 89.f;
+	}
+}
+
+void Camera::MousePassiveMotionInput(int x, int y, int prevX, int prevY) {
+	//float angleX{ -(x - prevX) / m_angleSpeed };
+	//ViewPointMove(angleX, m_cameraAxisY);
+	//m_cameraAngleX += angleX;
+
+	//if (m_cameraAngleY > -90.f and m_cameraAngleY < 90.f) {
+	//	float angleY{ -(y - prevY) / m_angleSpeed };
+	//	ViewPointMove(angleY, m_cameraAxisX);
+	//	m_cameraAngleY += angleY;
+	//}
+	//else if (m_cameraAngleY <= -90.f) {
+	//	m_cameraAngleY = -89.f;
+	//}
+	//else if (m_cameraAngleY >= 90.f) {
+	//	m_cameraAngleY = 89.f;
+	//}
+}
+
+void Camera::Move(const glm::vec3& moveVec) {
+	glm::vec3 m = moveVec * m_moveSpeed * m_deltaTime;
+	m_EYE += m;
+}
+
+void Camera::ViewPointMove(float moveAngle, const glm::vec3& axis) {
+	glm::vec4 at{ m_AT, 1.f };
+	m_AT = glm::normalize(glm::rotate(m_AT, glm::radians(moveAngle), axis));
+}
+
+void Camera::Init() {
+
+}
+
+void Camera::Update(float deltaTime) {
+	m_deltaTime = deltaTime;
+
+	// 카메라 기저 계산 순서 -> n, u, v -> 카메라 z, x, y축
+	m_cameraAxisZ = glm::normalize(-m_AT);
+	m_cameraAxisX = glm::normalize(glm::cross(m_UP, m_cameraAxisZ));
+	m_cameraAxisY = glm::normalize(glm::cross(m_cameraAxisZ, m_cameraAxisX));
+	OBJECTSHADER->UseProgram();
+
+	// for all lighting
+	OBJECTSHADER->SetUniformVec3("viewPosition", m_EYE);
+
+	if (m_spotLightingOn) {
+		//// for flash lighting values
+		OBJECTSHADER->SetUniformVec3("spotLight.position", m_EYE);
+		OBJECTSHADER->SetUniformVec3("spotLight.direction", m_AT);
+
+		OBJECTSHADER->SetUniformFloat("spotLight.cutOff", std::cosf(glm::radians(7.5f)));
+		OBJECTSHADER->SetUniformFloat("spotLight.outerCutOff", std::cosf(glm::radians(17.5f)));
+	}
+
+	OBJECTSHADER->UnUseProgram();
+}
+
+void Camera::Render() {
+	m_view = glm::lookAt(m_EYE, m_EYE + m_AT, m_UP);
+}
--- Graphics/GameWorld.cpp
+++ Graphics/GameWorld.cpp
@@ -1,2 +1,144 @@
 #include "pch.h"
-#include "GameWorld.h"
+#include "Graphics/GameWorld.h"
+#include "Graphics/Shader.h"
+#include "Graphics/Camera.h"
+#include "Model/ModelList.h"
+
+#include "Util/TextureComponent.h"
+
+GameWorld::GameWorld() { }
+
+GameWorld::~GameWorld() { }
+
+bool GameWorld::IsInited() const {
+	return m_isInited;
+}
+
+void GameWorld::SetWindowInfo(std::shared_ptr<struct WindowInfo>& winInfo) {
+	m_windowInfo = winInfo;
+	CalcPerspectiveMat();
+}
+
+void GameWorld::CalcPerspectiveMat() {
+	if (!m_isInited) {
+		return;
+	}
+
+	float aspect = m_windowInfo->fWidth / m_windowInfo->fHeight;
+	float halfFovy = m_fovy / 2.f;
+
+	m_perspectiveMatrix = glm::perspective(glm::radians(halfFovy), aspect, m_near, m_far);
+}
+
+void GameWorld::Input(unsigned char key, bool down) {
+	m_camera->Input(key, down);
+}
+
+void GameWorld::SpecialInput(int key, bool down) {
+	if (down) {
+		if (key == GLUT_KEY_F1) {
+			OBJECTSHADER->UseProgram();
+			OBJECTSHADER->UnUseProgram();
+		}
+
+		if (key == GLUT_KEY_F2) {
+			OBJECTSHADER->UseProgram();
+			OBJECTSHADER->UnUseProgram();
+		}
+
+		if (key == GLUT_KEY_F3) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
+
+		if (key == GLUT_KEY_F4) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+	}
+}
+
+void GameWorld::MouseMotionInput(int x, int y, int prevX, int prevY) {
+	m_camera->MouseMotionInput(x, y, prevX, prevY);
+}
+
+void GameWorld::MousePassiveMotionInput(int x, int y, int prevX, int prevY) {
+}
+
+void GameWorld::CreateShaderPrograms() {
+	OBJECTSHADER->CreateShaderProgram();
+	LIGHTOBJECTSHADER->CreateShaderProgram();
+	PARTICLESHADER->CreateShaderProgram();
+	TERRAINSHADER->CreateShaderProgram();
+	BACKGROUNDSHADER->CreateShaderProgram();
+}
+
+void GameWorld::SetPerspectiveAllShader() {
+	BACKGROUNDSHADER->SetUniformMat4("perspectiveMat", m_perspectiveMatrix);
+	TERRAINSHADER->SetUniformMat4("perspectiveMat", m_perspectiveMatrix);
+	PARTICLESHADER->SetUniformMat4("perspectiveMat", m_perspectiveMatrix);
+	OBJECTSHADER->SetUniformMat4("perspectiveMat", m_perspectiveMatrix);
+	LIGHTOBJECTSHADER->SetUniformMat4("perspective", m_perspectiveMatrix);
+}
+
+void GameWorld::SetViewMatAllShader() {
+	glm::mat4 cameraViewMatrix{ m_camera->GetViewMat() };
+
+	BACKGROUNDSHADER->SetUniformMat4("viewMat", glm::mat4(glm::mat3(cameraViewMatrix)));
+	TERRAINSHADER->SetUniformMat4("viewMat", cameraViewMatrix);
+	PARTICLESHADER->SetUniformMat4("viewMat", cameraViewMatrix);
+	OBJECTSHADER->SetUniformMat4("viewMat", cameraViewMatrix);
+	LIGHTOBJECTSHADER->SetUniformMat4("view", cameraViewMatrix);
+}
+
+void GameWorld::InitModelList() {
+	// 모델리스트를 생성하고 모델 불러오기
+	MODELLIST->Init();
+	MODELLIST->LoadModel("cube.obj");
+	//MODELLIST->LoadModel("cone.obj");
+	//MODELLIST->LoadModel("sphere.obj");
+	//MODELLIST->LoadModel("cylinder.obj");
+	//MODELLIST->LoadModel("earth.obj", "Earth_diffuse_512p.png");
+}
+
+void GameWorld::SetGLGraphicOptions() {
+	glPointSize(2.f);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+}
+
+void GameWorld::Init() {
+	SetGLGraphicOptions();
+
+	// 카메라 생성
+	m_camera = std::make_unique<Camera>();
+	m_camera->Init();
+
+	InitModelList();
+
+	// 투영 변환 행렬 계산
+	CalcPerspectiveMat();
+
+	m_isInited = true;
+	
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+void GameWorld::Update(float deltaTime) {
+	m_deltaTime = deltaTime;
+	m_camera->Update(m_deltaTime);
+}
+
+void GameWorld::Render() {
+
+	glClearColor(1.f, 1.f, 1.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+	m_camera->Render();
+	SetPerspectiveAllShader();
+	SetViewMatAllShader();
+
+	glViewport(0, 0, m_windowInfo->width, m_windowInfo->height);
+
+	glutSwapBuffers();
+}
